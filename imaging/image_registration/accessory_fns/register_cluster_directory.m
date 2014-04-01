459c53a8df25e0548b191bea20c15123f08e1953
Nicholas Sofroniew <sofroniewn@janelia.hhmi.org>
2014-04-01 18:47:40 -0400

    Bug Fix run on cluster
    Change name to run on cluster properly

--- imaging/image_registration/accessory_fns/register_cluster_directory.m
+++ imaging/image_registration/accessory_fns/register_cluster_directory.m
@@ -1,5 +1,7 @@
 function register_cluster_directory(data_dir,file_num)
 
+	fprintf('\n Start cluster register %s \n',file_num);
+
 	% Directory for registered images
 	type_name = 'registered';
 	folder_name = fullfile(data_dir,type_name);
@@ -30,6 +32,7 @@ function register_cluster_directory(data_dir,file_num)
 	im_align = im_raw(:,:,align_chan:num_chan:end);
 	num_frames = size(im_align,3);
 
+	fprintf('Compute shifts %s \n',file_num);
 	% compute shifts
 	shifts_raw = zeros(num_frames,2);
 	for ij = 1:num_frames
@@ -40,6 +43,7 @@ function register_cluster_directory(data_dir,file_num)
 		[shifts_raw(ij,:) tmp]= register_image(cur_im,ref_im);
 	end
 
+	fprintf('Do shifts %s \n',file_num);
 	% shift images 
 	im_adj_raw = zeros(size(im_raw),'single');
 	for ij = 1:size(im_raw,3)
@@ -47,6 +51,7 @@ function register_cluster_directory(data_dir,file_num)
 		im_adj_raw(:,:,ij) = func_im_shift(im_raw(:,:,ij),shifts_raw(ind,:));
 	end
 
+	fprintf('Resort data %s \n',file_num);
 	% resort data
 	im = cell(num_planes,num_chan);
 	im_aligned = cell(num_planes,num_chan);
@@ -62,13 +67,14 @@ function register_cluster_directory(data_dir,file_num)
 			mean_raw{ik,ij} = mean(im{ik,ij},3);
 			mean_aligned{ik,ij} = mean(im_aligned{ik,ij},3);
 			im{ik,ij} = uint16(im{ik,ij});
-			im_adj{ik,ij} = uint16(im_adj{ik,ij});
+			im_aligned{ik,ij} = uint16(im_aligned{ik,ij});
 			mean_raw{ik,ij} = uint16(mean_raw{ik,ij});
 			mean_aligned{ik,ij} = uint16(mean_aligned{ik,ij});
 			shifts{ik} = shifts_raw(ik:num_planes:end,:);
 		end
 	end
 
+	fprintf('Extract summary data %s \n',file_num);
 	% extract summary data
 	im_summary.props.num_frames = size(shifts_raw,1)/num_planes;
 	im_summary.props.num_planes = num_planes;
@@ -81,6 +87,7 @@ function register_cluster_directory(data_dir,file_num)
 	im_summary.mean_raw = mean_raw;
 	im_summary.mean_aligned = mean_aligned;
 
+	fprintf('Save data %s \n',file_num);
 	% save data
 	replace_start = strfind(base_name,'main');
 	replace_end = replace_start+4;
