039dfda869a55d1476b52cc35be0cb40c224f263
Nicholas Sofroniew <sofroniewn@janelia.hhmi.org>
2014-04-02 12:35:38 -0400

    Reposition cluster compile script

---
+++ imaging/preprocessing/image_registration/accessory_fns/register_cluster_directory_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf compiled
+mkdir compiled
+cd compiled
+/usr/local/matlab-2010a/bin/mcc -R -singleCompThread -C -m /groups/freeman/home/freemanj11/code/wgnr/imaging/preprocessing/image_registration/accessory_fns/register_cluster_directory.m -a /groups/freeman/home/freemanj11/code/wgnr/imaging/preprocessing/
+./register_cluster_directory
+cd ..
+echo "registration successfully compiled for cluster"
+chmod -R 777 compiled
