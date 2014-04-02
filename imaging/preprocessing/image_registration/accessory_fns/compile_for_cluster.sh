4ff1be5789c0550ba33d622284eba25e2ce4f57c
freeman <the.freeman.lab@gmail.com>
2014-04-02 13:31:51 -0400

    Combined multiple compilations into one shell script

--- imaging/preprocessing/image_registration/accessory_fns/compile_for_cluster.sh
+++ imaging/preprocessing/image_registration/accessory_fns/compile_for_cluster.sh
@@ -5,6 +5,8 @@ mkdir compiled
 cd compiled
 /usr/local/matlab-2010a/bin/mcc -R -singleCompThread -C -m /groups/freeman/home/freemanj11/code/wgnr/imaging/preprocessing/image_registration/accessory_fns/register_cluster_directory.m -a /groups/freeman/home/freemanj11/code/wgnr/imaging/preprocessing/
 ./register_cluster_directory
+/usr/local/matlab-2010a/bin/mcc -R -singleCompThread -C -m /groups/freeman/home/freemanj11/code/wgnr/imaging/preprocessing/image_registration/accessory_fns/save_text_cluster.m -a /groups/freeman/home/freemanj11/code/wgnr/imaging/preprocessing/
+./save_text_cluster
 cd ..
-echo "registration successfully compiled for cluster"
+echo "code successfully compiled for cluster"
 chmod -R 777 compiled
