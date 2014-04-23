c5b5027d2df4512c6b591d93925b2c1e61447c2b
Nicholas Sofroniew <sofroniewn@janelia.hhmi.org>
2014-04-23 16:45:09 -0400

    Add spark r2 local corr overlay
    Add plotting function so can look at r2 of regression overlaid on r2 of
    local corr

---
+++ imaging/preprocessing/image_viewer/accessory_fns/plot_functions/plot_spark_regression_stats_overlay.m
@@ -0,0 +1,41 @@
+function im_comb = plot_spark_regression_stats_overlay(plot_axes,im_session,trial_num,chan_num,plot_planes,clim,plot_on)
+
+axes(plot_axes);
+colormap(gca,'gray');
+    
+num_planes = length(plot_planes);
+plane_rep = ceil(sqrt(num_planes));
+im_comb = zeros(plane_rep*im_session.ref.im_props.height,plane_rep*im_session.ref.im_props.width,3);
+
+cur_ind = im_session.spark_output.regressor.cur_ind;
+im_array = im_session.spark_output.regressor.stats{cur_ind};
+im_array_lc = im_session.spark_output.localcorr;
+
+if ~isempty(im_array) && ~isempty(im_array_lc)
+for ij = 1:num_planes
+	row_val = mod(ij-1,plane_rep);
+	col_val = floor((ij-1)/plane_rep);
+	start_x = 1 + row_val*im_session.ref.im_props.height;
+	start_y = 1 + col_val*im_session.ref.im_props.height;
+	im_use = im_array{plot_planes(ij),chan_num}*10;
+	im_comb(start_y:start_y+im_session.ref.im_props.height-1,start_x:start_x+im_session.ref.im_props.width-1,1) = im_use;
+	%im_comb(start_y:start_y+im_session.ref.im_props.height-1,start_x:start_x+im_session.ref.im_props.width-1,2) = im_use;
+	im_use = im_array_lc{plot_planes(ij),chan_num};
+	im_comb(start_y:start_y+im_session.ref.im_props.height-1,start_x:start_x+im_session.ref.im_props.width-1,3) = im_use;
+
+end
+end
+
+clim = clim/1000;
+im_comb = (im_comb - clim(1))/clim(2);
+im_comb(im_comb>1) = 1;
+im_comb(im_comb<0) = 0;
+
+
+if plot_on == 1
+	imshow(im_comb)
+	set(gca,'xtick',[])
+	set(gca,'ytick',[])
+end
+
+end
